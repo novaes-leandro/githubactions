07fac5ad350d512889531b0413992bb206883ba1
¨Leandro <lsmn@hotmail.com.br>
2023-06-12 13:24:07 -0300

    feat: alteracao da mensagem de erro

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado a soma é inválido: Resultado %d. Esperado: %d", total, 30)
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
